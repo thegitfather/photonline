57f6ee9355079445d017e43a3cebbee08afffff8
thegitfather <baranowski@joinpoint.org>
2016-04-19 19:43:22 +0200

    gallery view: ngDialog with nexp/prev buttons

--- client/app/gallery/galleryItem/galleryItem.directive.js
+++ client/app/gallery/galleryItem/galleryItem.directive.js
@@ -12,19 +12,34 @@ angular.module('photoboxApp')
 
       link: function (scope, element, attrs) {
         var ngDialog = scope.$parent.vm.ngDialog;
+        scope.windowHeight = window.innerHeight;
+        scope.originalFilename = scope.photo.originalFilename;
 
+        element.find("a").on("click", function(e) {
+          ngDialog.closeAll();
 
+          scope.nextElement = document.getElementById( "gallery-item-" + parseInt(scope.$index + 1) );
+          scope.prevElement = document.getElementById( "gallery-item-" + parseInt(scope.$index - 1) );
 
-        element.on("click", function(e) {
-          // var body = angular.element(document).find('html');
-          // console.log(window.innerHeight);
-          scope.windowHeight = window.innerHeight;
           ngDialog.open({
             template: 'app/gallery/galleryItem/photo.template.html',
             scope: scope,
             className: 'ngdialog-theme-photobox'
           });
-        })
+        });
+
+        scope.openNext = function() {
+          if (scope.nextElement !== null) {
+            angular.element(scope.nextElement).find("a")[0].click();
+          }
+        };
+
+        scope.openPrev = function() {
+          if (scope.prevElement !== null) {
+            angular.element(scope.prevElement).find("a")[0].click();
+          }
+        };
+
       }
     };
   });
