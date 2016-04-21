20290164519aff383fa65375f4506477deac2e06
thegitfather <baranowski@joinpoint.org>
2016-04-21 17:22:02 +0200

    upload limit 6 MB

--- server/config/environment/shared.js
+++ server/config/environment/shared.js
@@ -7,9 +7,7 @@ exports = module.exports = {
   // limits for angularFileUpload
   uploadLimits: {
     "minFileSize": 10240, // 10 KB
-    "maxFileSize": 8388608, // 8 MB
+    "maxFileSize": 6291456, // 6 MB
     "queueLimit": 100
-  },
-
-  poolPath: './uploads/pool/'
+  }
 };
