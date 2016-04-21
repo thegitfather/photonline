26534ef5e4a11315b5a809d37934553db555e6c0
thegitfather <baranowski@joinpoint.org>
2016-04-21 17:21:49 +0200

    just remove all files

--- scripts/clean-db.sh
+++ scripts/clean-db.sh
@@ -5,7 +5,8 @@ command -v mongo >/dev/null 2>&1 || { echo "please make sure 'mongo' is properly
 BASEDIR=$(dirname $0)
 
 mongo "${BASEDIR}"/clean-db.js
-#rm -fr "${BASEDIR}"/../uploads
-find "${BASEDIR}"/../uploads -mindepth 1 -maxdepth 1 -type d -exec rm -fr {} \;
+
+# just remove files so directories stay (initially created at server start)
+find "${BASEDIR}"/../uploads -mindepth 1 -maxdepth 2 -type f -exec rm -fv {} \;
 
 exit 0
