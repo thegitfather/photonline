9a3972c114fb2998a66d4d2f6b21f114bb3cf2f1
thegitfather <baranowski@joinpoint.org>
2016-04-26 14:41:35 +0200

    update desc

--- scripts/clean-db.sh
+++ scripts/clean-db.sh
@@ -3,7 +3,7 @@
 command -v mongo >/dev/null 2>&1 || { echo "please make sure 'mongo' is properly installed. aborting..." >&2; exit 1; }
 
 if [ "$#" -ne 1 ]; then
-  echo "Usage: `basename $0` <host>/<db-name>"
+  echo "Usage: `basename $0` <host[:port]>/<db-name>"
   exit 1
 fi
 
